15f371bf5c8ad2e8b699d1953b68d643f4e1e958
Christopher Ras <chris@MacBook-Pro.local>
2016-11-24 21:15:19 +0100

    Database functionality added.

---
+++ chrisras-pset4/DatabaseHelper.swift
@@ -0,0 +1,90 @@
+//
+//  DatabaseHelper.swift
+//  chrisras-pset4
+//
+//  Created by Christopher Ras on 24/11/2016.
+//  Copyright © 2016 Chris Ras. All rights reserved.
+//
+
+import Foundation
+import SQLite
+
+class DatabaseHelper {
+    
+    private let tasks = Table("tasks")
+    private let id = Expression<Int64>("id")
+    private let entry = Expression<String?>("entry")
+    private var db: Connection?
+    
+    init?() {
+        do {
+            try setupDatabase()
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    private func setupDatabase() throws {
+        let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+        
+        do {
+            db = try Connection("\(path)/db.sqlite3")
+            try createTable()
+        } catch {
+            throw error
+        }
+    }
+    
+    private func createTable() throws {
+        do {
+            try db!.run(tasks.create(ifNotExists: true) {
+                t in
+                
+                t.column(id, primaryKey: .autoincrement)
+                t.column(entry)
+            })
+        } catch {
+            throw error
+        }
+    }
+    
+    func addToDB(entry: String) throws {
+        let insert = tasks.insert(self.entry <- entry)
+        do {
+            let rowId = try db!.run(insert)
+            print(rowId)
+        } catch {
+            throw error
+        }
+    }
+    
+    func readFromDB() throws -> Array<String> {
+        var currentTasks = [String]()
+        do {
+            for task in try db!.prepare(tasks) {
+                currentTasks.append(task[entry]!)
+            }
+            
+            return currentTasks
+            
+        } catch {
+            throw error
+        }
+    }
+    
+    func deleteFromDB(task: String) throws {
+        let toBeDeleted = tasks.filter(entry == task)
+        do {
+            if try db!.run(toBeDeleted.delete()) > 0 {
+                print("\(task) deleted")
+            } else {
+                print("\(task) not found")
+            }
+            
+        } catch {
+            print(error)
+        }
+        
+    }
+}
--- chrisras-pset4/ViewController.swift
+++ chrisras-pset4/ViewController.swift
@@ -12,11 +12,23 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var taskTextField: UITextField!
     
-    var tasks = ["Lorem", "Ipsum", "Dolor"]
+    private let db = DatabaseHelper()
+    var tasks = [String]()
     override func viewDidLoad() {
         
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if db == nil{
+            print("error")
+        } else {
+            do {
+                try tasks = db!.readFromDB()
+                tableView.reloadData()
+            } catch {
+                print(error)
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,14 +50,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == UITableViewCellEditingStyle.delete {
-            tasks.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
+            
+            //tasks.remove(at: indexPath.row)
+            do {
+                try db!.deleteFromDB(task: tasks[indexPath.row])
+                //tasks.remove(at: indexPath.row)
+                try tasks = db!.readFromDB()
+            } catch {
+                print(error)
+            }
+            tableView.reloadData()
+            //tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
         }
     }
     
     @IBAction func addTaskButton(_ sender: Any) {
-        if taskTextField.text != nil {
-            tasks.append(taskTextField.text!)
+        if taskTextField.text != nil && taskTextField.text != "" {
+            //tasks.append(taskTextField.text!)
+            do {
+                try db!.addToDB(entry: taskTextField.text!)
+                try tasks = db!.readFromDB()
+            } catch {
+                print(error)
+            }
             tableView.reloadData()
         }
     }
